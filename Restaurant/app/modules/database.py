48d8f7e3d6593912769994459869baf536a9e7a3
Macbook <macbook@192.168.0.102>
2026-08-20 14:35:06 +0545

    corrected

--- Restaurant/app/modules/database.py
+++ Restaurant/app/modules/database.py
@@ -8,10 +8,12 @@ class Database:
         try:
             self.__connection = pymysql.connect(
                 host=config.MYSQL_HOST,
+                port=config.MYSQL_PORT,
                 user=config.MYSQL_USER,
                 password=config.MYSQL_PASSWORD,
                 database=config.MYSQL_DATABASE,
                 cursorclass=pymysql.cursors.DictCursor,
+                ssl={"ssl": {}}
             )
 
             print("Database connected successfully!")
--- Restaurant/config.py
+++ Restaurant/config.py
@@ -5,6 +5,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "random-secret-key")
 
 # MySQL DATABASE CONFIGURATION (Reads from Vercel Environment Variables)
 MYSQL_HOST = os.environ.get("MYSQL_HOST", "localhost")
+MYSQL_PORT = int(os.environ.get("MYSQL_PORT", 3306))
 MYSQL_USER = os.environ.get("MYSQL_USER", "root")
 MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "@rujab0011@")
 MYSQL_DATABASE = os.environ.get("MYSQL_DATABASE", "restaurant_management")
